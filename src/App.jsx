79a9d0fce954541cad57461e7e270b2308452dbb
efzr <emerson.zapatarivas@gmail.com>
2024-04-16 14:47:45 -0600

    carrito en proceso

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 import Footer from "./components/Footer";
+import { DB } from "./data/db";
 
 function App() {
+  const [guitarras, setGuitarras] = useState(DB);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitarra) => guitarra.id === item.id);
+    if (itemExists >= 0) {
+      const newCart = cart.map((guitarra) => {
+        if (guitarra.id === item.id) {
+          guitarra.quantity += 1;
+        }
+        return guitarra;
+      });
+      setCart(newCart);
+    } else {
+      item.quantity = 1;
+      setCart((prevState) => [...prevState, item]);
+    }
+  }
+
   return (
     <>
       <Header />
@@ -11,12 +32,13 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          {guitarras.map((guitarra) => (
+            <Guitar
+              key={guitarra.id}
+              guitarra={guitarra}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
       </main>
 
